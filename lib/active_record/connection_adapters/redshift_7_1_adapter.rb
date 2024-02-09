6f6c051e729060909f1c5177a21b3035ef9a1da6
Christian Schmidt <github@chsc.dk>
2024-02-09 16:05:37 +0100

    Use kwargs for PG::Coder.new (#20)

--- lib/active_record/connection_adapters/redshift_7_1_adapter.rb
+++ lib/active_record/connection_adapters/redshift_7_1_adapter.rb
@@ -707,7 +707,7 @@ module ActiveRecord
             PG::TextDecoder::TimestampWithoutTimeZone
           end
 
-        @timestamp_decoder = decoder_class.new(@timestamp_decoder.to_h)
+        @timestamp_decoder = decoder_class.new(**@timestamp_decoder.to_h)
         @raw_connection.type_map_for_results.add_coder(@timestamp_decoder)
         @default_timezone = ActiveRecord.default_timezone
 
